ea83fa777ab7411be54ac75d61b0db0e009c0c44
Joni Mäkinen <joni-makinen@live.fi>
2025-02-15 15:25:04 +0200

    Add functionality to save records to Local Storage and functionality to fetch records from Local Storage.

--- scripts/make-entry.js
+++ scripts/make-entry.js
@@ -3,39 +3,60 @@ function makeEntry() {
   entryButton.remove();
   viewButton.remove();
 
-  // 2. Create the hours input field
+  // 2. Create instructions
+  const instructions = document.createElement("p");
+  instructions.innerText = "How many hours did you work and on what course?";
+
+  // 3. Create the hours input field
   const input = document.createElement("input");
   input.value = "Hours";
   input.addEventListener("focus", (event) => {
     input.value = null;
   });
 
-  // 3. Create the course selection dropdown list
-  const list = document.createElement("form");
+  // // 4. Create the course selection dropdown list
+  // const list = document.createElement("form");
 
-  // 4. Insert elements to content div
+  // 5. Create the submit button and add a listener
+  const submitButton = document.createElement("button");
+  submitButton.innerText = "Submit";
+  submitButton.addEventListener("click", function () {
+    handleSubmit(input);
+  });
+
+  // 6. Insert elements to content div
+  content.append(instructions);
   content.append(input);
-  content.append(list);
+  // content.append(list);
+  content.append(submitButton);
 }
 
-// function saveInput() {
-//   localStorage.setItem("name", "Joni");
-//   console.log(localStorage);
-// }
+function handleSubmit(input) {
+  // 7. Validate submit values
+  // Returns a boolean value
+  const validated = validateInput(input);
+  if (validated) {
+    console.log("Validation: " + validated);
+    saveInput(input);
+  } else {
+    console.log("Validation: " + validated);
+  }
+}
 
-// function validateInput() {
-//   if (input.value) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function validateInput(input) {
+  // Check if input.value is a number
+  if (!isNaN(input.value) && input.value !== "") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// // Returns a boolean value
-// const validated = validateInput();
-// if (validated) {
-//   console.log(validated);
-//   saveInput();
-// } else {
-//   console.log(validated);
-// }
+function saveInput(input) {
+  // Add a prefix to hours
+  // This is used when fetching everything from Local Storage
+  // to see if an item is really a curriculus record
+  const item = "curriculus" + input.value;
+  // Save hours to Local Storage
+  localStorage.setItem(item, input.value);
+}
--- scripts/view-records.js
+++ scripts/view-records.js
@@ -3,15 +3,37 @@ function viewRecords() {
   entryButton.remove();
   viewButton.remove();
 
-  // 2. Create the record list
-  const records = document.createElement("table");
-  records.insertRow();
-  records.insertRow();
-  records.insertRow();
-  records.querySelector("tbody").querySelectorAll("tr")[0].innerText = "a";
-  records.querySelector("tbody").querySelectorAll("tr")[1].innerText = "b";
-  records.querySelector("tbody").querySelectorAll("tr")[2].innerText = "c";
+  // 2. Fetch records from Local Storage
+  const recordList = [];
+  // Go through all items in Local Storage
+  // If an item starts with "curriculus", append it to recordList
+  for (item in localStorage) {
+    if (
+      item[0] === "c" &&
+      item[1] === "u" &&
+      item[2] === "r" &&
+      item[3] === "r" &&
+      item[4] === "i" &&
+      item[5] === "c" &&
+      item[6] === "u" &&
+      item[7] === "l" &&
+      item[8] === "u" &&
+      item[9] === "s"
+    ) {
+      recordList.push(item);
+    }
+  }
 
-  // 3. Insert table to content div
+  // 3. Create the record table
+  const records = document.createElement("table");
+  for (let i = 0; i < recordList.length - 1; i++) {
+    records.insertRow();
+    records.querySelector("tbody").querySelectorAll("tr")[i].innerText =
+      recordList[i];
+  }
+
+  console.log(records);
+
+  // 4. Insert table to content div
   content.append(records);
 }
